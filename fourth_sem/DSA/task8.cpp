a67f0e0788e2963420e9a53cb9b6faa99f908464
Dmitriy <dmkrch1@gmail.com>
2021-04-18 19:38:10 +0300

    dsa task1 and task8 added

---
+++ fourth_sem/DSA/task8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    std::string input_line;
+    std::cin >> input_line;
+    int length = (int)input_line.length();
+
+    bool isSymbolRepeats = true;
+
+    for (int i = 0, j = length-1; i <= j; i++, j--) 
+    {
+        if (input_line[i] != input_line[j]) 
+        {
+            std::cout << length;
+            return 0;
+        }
+        if (isSymbolRepeats && input_line[i] != input_line[0])
+            isSymbolRepeats = false;
+    }
+
+    if (isSymbolRepeats)
+        std::cout << "-1";
+    else
+        std::cout << length-1;
+    
+    return 0;
+}
