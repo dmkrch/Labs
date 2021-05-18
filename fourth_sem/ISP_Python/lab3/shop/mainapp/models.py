001cfc37010078cb413a11fd7249c1ce7f0a9bde
Dmitriy <dmkrch1@gmail.com>
2021-05-19 01:03:55 +0300

    some modifications with template. Now using filter to make general html templates showing in product_view

--- fourth_sem/ISP_Python/lab3/shop/mainapp/models.py
+++ fourth_sem/ISP_Python/lab3/shop/mainapp/models.py
@@ -69,6 +69,8 @@ class Cart(models.Model):
     products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
+    in_order = models.BooleanField(default=False)
+    for_anonymous_user = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.id)
