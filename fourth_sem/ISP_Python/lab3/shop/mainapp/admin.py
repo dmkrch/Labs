5a168b8e9d335554bfe92c23b436610468fd93cd
Dmitriy <dmkrch1@gmail.com>
2021-05-19 13:22:03 +0300

    added admin panel form changes. Now sd in smarphones is not showed if null

--- fourth_sem/ISP_Python/lab3/shop/mainapp/admin.py
+++ fourth_sem/ISP_Python/lab3/shop/mainapp/admin.py
@@ -1,30 +1,23 @@
-from django.forms import ModelChoiceField, ModelForm, ValidationError
+from django.forms import ModelChoiceField, ModelForm
 from django.contrib import admin
 from .models import *
-from django.utils.safestring import mark_safe
-from PIL import Image
 
-
-class NotebookAdminForm(ModelForm):
-
-    MIN_RESOLUTION = (400, 400)
-
+class SmartphoneAdminForm(ModelForm):
     def __int__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = mark_safe('<span_style="color:red;">Загружайте изображения с минимальным разрешением {}x{}</span'.format(
-            *self.MIN_RESOLUTION))
+        instance = kwargs.get('instance')
+        if not instance.sd:
+            self.fields['sd_volume_max'].widget.attrs.update({
+                'readonly': True, 'style': 'background: lightgray'
+            })
 
-    def clean_image(self):
-        image = self.cleaned_data['image']
-        img = Image.open(image)
-        min_height, min_width = self.MIN_RESOLUTION
-        if img.height < min_height or img.width < min_width:
-            raise ValidationError('Разрешение изображения меньше минимального!')
-        return image
+        def clean(self):
+            if not self.cleaned_data['sd']:
+                self.cleaned_data['sd_volume_max'] = None
+            return self.cleaned_data
 
 
 class NotebookAdmin(admin.ModelAdmin):
-    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
@@ -33,6 +26,9 @@ class NotebookAdmin(admin.ModelAdmin):
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
+    change_form_template = 'admin.html'
+    form = SmartphoneAdminForm
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
             return ModelChoiceField(Category.objects.filter(slug='smartphones'))
