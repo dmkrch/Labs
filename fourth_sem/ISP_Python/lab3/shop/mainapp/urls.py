68d206e16da14dbc16c3f5681284ae361f88af43
Dmitriy <dmkrch1@gmail.com>
2021-05-21 23:46:13 +0300

    renamed lab3

--- fourth_sem/ISP_Python/lab3/shop/mainapp/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import test_view, ProductDetailView
-
-urlpatterns = [
-    path('', test_view, name='base'),
-    path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail')
-]
