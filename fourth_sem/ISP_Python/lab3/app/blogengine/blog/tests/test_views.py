7525b5e0401bc258c5b55f3281d52ee2b7f4a946
Dmitriy <dmkrch1@gmail.com>
2021-05-26 12:46:38 +0300

    coverage now is 80%

--- fourth_sem/ISP_Python/lab3/app/blogengine/blog/tests/test_views.py
+++ fourth_sem/ISP_Python/lab3/app/blogengine/blog/tests/test_views.py
@@ -2,17 +2,52 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from blog.models import *
 import json
+from django.contrib.auth.models import User
 
 
 class TestViews(TestCase):
     def setUp(self):
-        self.client = Client()
-        self.posts_list_url = reverse('posts_list_url')
+        user = User()
+        user.username = "admin"
+        user.password = "admin1"
+        user.save()
+        self.client.force_login(user)
 
-    def test_post_detail_view_GET(self):
+        self.posts_list_url = reverse('posts_list_url')
+        self.tags_list_url = reverse('tags_list_url')
+        self.tag_create_url = reverse('tag_create_url')
+        self.post_create_url = reverse('post_create_url')
+        self.login_url = reverse('login_url')
+        self.register_url = reverse('register_url')
+
+    def test_posts_list_view_GET(self):
         response = self.client.get(self.posts_list_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/index.html')
 
-    def test_post_detail_view_POST(self):
-        client = Client()
+    def test_tags_list_view_GET(self):
+        response = self.client.get(self.tags_list_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/tags_list.html')
+
+    def test_post_create_view_GET(self):
+        response = self.client.get(self.post_create_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/post_create.html')
+
+    def test_tag_create_view_GET(self):
+        response = self.client.get(self.tag_create_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/tag_create.html')
+
+    def test_login_view_GET(self):
+        self.client.logout()
+        response = self.client.get(self.login_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/login.html')
+
+    def test_register_view_GET(self):
+        self.client.logout()
+        response = self.client.get(self.register_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/register.html')
